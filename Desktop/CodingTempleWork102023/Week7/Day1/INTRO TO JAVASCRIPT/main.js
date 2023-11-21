52df313d0f3c9fdda09dd859a6342665b410f36c
HeatherSmallwood <heathersmallwood.hs@gmail.com>
2023-11-21 16:03:04 -0500

    final

--- Desktop/CodingTempleWork102023/Week7/Day1/INTRO TO JAVASCRIPT/main.js
+++ Desktop/CodingTempleWork102023/Week7/Day1/INTRO TO JAVASCRIPT/main.js
@@ -273,10 +273,6 @@ let dog_string = "Hello Max, my name is Dog, and I have purple eyes!";
 let dog_names = ["Max", "HAS", "PuRple", "dog"];
 
 
-
-
-
-
 //Call method here with parameters
 findWords(dog_string, dog_names);
 
